13a4b29bb6b2819460c081eebc8b20b97baa369f
Aditi Polkam <aditipolkam@gmail.com>
2022-03-14 22:48:53 +0530

    [ADD] basic contract to mint NFT

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract. LOL");
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/A3MK");
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+        _tokenIds.increment();
+    }
+}
